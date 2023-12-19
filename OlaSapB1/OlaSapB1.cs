30db87bc09623e7461773224fd372e98e9da96c6
S o u z a <76980077+IgorSouzaOliveira@users.noreply.github.com>
2023-12-18 22:59:30 -0300

    First Commit
    Conexão com SAPB1 e mensagem de apresentação.

---
+++ OlaSapB1/OlaSapB1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlaSapB1
+{
+    public class OlaSapB1
+    {
+        private SAPbouiCOM.Application oApplication;
+        private void setApplication()
+        {
+            SAPbouiCOM.SboGuiApi oSboGuiApi = null;
+            string sConnectionString = null;
+            oSboGuiApi = new SAPbouiCOM.SboGuiApi();
+            sConnectionString = System.Convert.ToString(Environment.GetCommandLineArgs().GetValue(1));
+            oSboGuiApi.Connect(sConnectionString);
+            oApplication = oSboGuiApi.GetApplication(-1);
+
+        }
+
+        public OlaSapB1()
+        {
+            setApplication();
+
+            oApplication.MessageBox("Olá Sap B1", 1, "OK", "", "");
+        }
+    }
+}
